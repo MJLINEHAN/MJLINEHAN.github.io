82c58d9fcba48e103a135b9bcb149ca4643296e2
MJLINEHAN <68347050+MJLINEHAN@users.noreply.github.com>
2020-07-16 11:07:56 -0400

    M

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -270,7 +270,7 @@ var onPointerMove = function(evt) {
     if (doHover) {
         if (popupText) {
             overlayPopup.setPosition(coord);
-            content.innerHTML = paysagelink+ '<iframe style="width:100%;height:110px;border:0px;"src="' + popupfile+'"></iframe>';
+            content.innerHTML = paysage+ '<iframe style="width:100%;height:110px;border:0px;"src="' + popupfile+'"></iframe>';
             container.style.display = 'block';        
         } else {
             container.style.display = 'none';
